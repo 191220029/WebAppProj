db56b4af3c24edf8bfe2e65cf7c7b7d9b68ab2a5
PetraEcho <61217622+PetraEcho@users.noreply.github.com>
2022-03-27 14:28:11 +0800

    3.27 初步制作了logon注册界面

--- src/main/java/com/springboot/sprintboottest/controller/LogonController.java
+++ src/main/java/com/springboot/sprintboottest/controller/LogonController.java
@@ -1,12 +1,32 @@
 package com.springboot.sprintboottest.controller;
 
+import com.springboot.sprintboottest.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class LogonController {
     @GetMapping("/logon")
     public String Logon(){
         return "logon";
+    }
+
+    @Autowired
+    UserRepository userRepository;
+    @RequestMapping("/user/logon")
+    public String userLogon(
+            @RequestParam("username")String username,
+            Model model, HttpSession session){
+        if(username.isEmpty()){
+            model.addAttribute("msg", "用户名不能为空");
+            return "logon";
+        }
+        return "login";
     }
 }
