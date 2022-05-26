3983f8b5c449b013d23a5ef9d3286cb71272d475
191220029 <1830970417@qq.com>
2022-05-26 09:45:42 +0800

    Create RoleTable_Admin_Controller.java

---
+++ src/main/java/com/springboot/sprintboottest/controller/RoleTable_Admin_Controller.java
@@ -0,0 +1,37 @@
+package com.springboot.sprintboottest.controller;
+
+import com.springboot.sprintboottest.Entity.roleBaseInfo;
+import com.springboot.sprintboottest.Repository.RoleBaseInfoRepository;
+import com.springboot.sprintboottest.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+public class RoleTable_Admin_Controller {
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    RoleBaseInfoRepository roleBaseInfoRepository;
+    @GetMapping("/admin/roleTable")
+    public String adminRoleTable(){
+        //TODO: 返回管理员搜索角色卡页面的html文件名
+        return null;
+    }
+    @ResponseBody
+    @RequestMapping("/admin/roleTable/search")
+    public List<roleBaseInfo> adminSearchRole(
+            @RequestParam("adminId") Integer adminId,
+            @RequestParam("mod") String mod,
+            @RequestParam("keyword") String keyword
+    )
+    {
+        //TODO: 返回查找结果
+        return null;
+    }
+}
