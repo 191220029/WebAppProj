9b25dabdbbac4ee41967c68d19acfcb04ba4e5a8
191220029 <1830970417@qq.com>
2022-05-27 09:45:19 +0800

    RoleBaseInfo JPA Interface for admin
    添加了管理员查询角色卡基本信息所需的JPA接口

--- src/main/java/com/springboot/sprintboottest/Repository/RoleBaseInfoRepository.java
+++ src/main/java/com/springboot/sprintboottest/Repository/RoleBaseInfoRepository.java
@@ -15,4 +15,12 @@ public interface RoleBaseInfoRepository extends JpaRepository<roleBaseInfo, Inte
     @Query(nativeQuery = true, value = "SELECT * FROM kesulu_online.role_base_info WHERE role_card_id = :RoleCardId")
     roleBaseInfo findRolebaseinfo_id(@Param("RoleCardId") Integer RoleCardId);
 
+    @Query(nativeQuery = true, value = "SELECT * FROM kesulu_online.role_base_info WHERE role_name LIKE \"%:roleName%\"")
+    List<roleBaseInfo> findroleBaseInfosByName(@Param("roleName") String roleName);
+
+
+    @Query(nativeQuery = true, value = "SELECT * FROM kesulu_online.role_base_info WHERE role_name LIKE \"%:roleName%\" AND role_card_player_id = :Id")
+    List<roleBaseInfo> findroleBaseInfosByNameAndId(@Param("roleName") String roleName,
+                                                    @Param("Id") Integer Id);
+
 }
